3ffc11ad9292ed8a75471a7a5bdcc16c0f469a38
Subhi YD <subhy809fr@gmail.com>
2022-10-25 18:03:49 +0200

    4e commit : rajout ReactiveFormsModule, customers.component.html, handleSearchCustomer, customersService, variable environment

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {CustomerService} from "../services/customer.service";
 import {catchError, Observable, throwError} from "rxjs";
 import {Customer} from "../model/customer.model";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-customers',
@@ -11,23 +12,33 @@ import {Customer} from "../model/customer.model";
 export class CustomersComponent implements OnInit {
   customers$!: Observable<Customer[]>;
   errorMessage!: Object;
+  searchFormGroup: FormGroup | undefined;
 
-  constructor(private customerService: CustomerService) { }
+  constructor(private customerService: CustomerService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    /*
-    this.http.get("http://localhost:8086/customers").subscribe(data => {
-    this.customers = data;
-    }, error => {
-      console.log(error);
-    });*/
 
-    this.customers$ = this.customerService.getCustomers().pipe (
+    this.searchFormGroup = this.fb.group({
+      keyword : this.fb.control("")
+    });
+
+   /* this.customers$ = this.customerService.getCustomers().pipe (
       catchError(err => {
         this.errorMessage = err.message;
 
         return throwError(err);
       })
-    );
+    );*/
+    this.handleSearchCustomer();
+  }
+
+  handleSearchCustomer() {
+    let kw = this.searchFormGroup?.value.keyword;
+    this.customers$ = this.customerService.searchCustomers(kw).pipe(
+      catchError(err => {
+        this.errorMessage = err.message;
+        return throwError(err);
+      })
+    )
   }
 }
--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -2,17 +2,23 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Customer} from "../model/customer.model";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
-
+  //backendHost: string = "http://localhost:8086";
   constructor(private http: HttpClient) { }
 
   public getCustomers(): Observable<Customer[]> {
 
-   return  this.http.get<Customer[]>("http://localhost:8086/customers");
+   return  this.http.get<Customer[]>(environment.backendHost + "/customers");
    //return  this.http.get<Customer[]>("http://localhost:8086/customer"); //pour les gestions des erreurs, endpoint n'existe pas.
+  }
+
+  public searchCustomers(keyword: string) {
+
+    return  this.http.get<Customer[]>(environment.backendHost + "/customers/search?keyword=" + keyword);
   }
 }
